8f708e6f17b5a6c7ca07a5b539ce86744ceabf18
jdavi <jdavid94@hotmail.com>
2024-10-01 17:52:29 +0200

    callbacks, async, and sync

--- src/main/java/com/kafka/spring/Application.java
+++ src/main/java/com/kafka/spring/Application.java
@@ -1,24 +1,50 @@
 package com.kafka.spring;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.KafkaListeners;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.List;
+
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	public static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	@KafkaListener(topics = "devs4j-topic", groupId = "devs4j-group")
-	public void listen(String message) {
-		log.info("Message receive {} ", message);
+	@KafkaListener(topics = "devs4j-topic", containerFactory = "listenerContainerFactory" , groupId = "devs4j-group"
+	, properties = {"max.poll.interval.ms:4000", "max.poll.records:10"})
+	public void listen(List<ConsumerRecord<String, String>> messages) {
+		log.info("Start reading messages");
+		for	(ConsumerRecord<String, String> message:messages) {
+			log.info("Partion = {}, Offset = {}, Key = {}, Value = {}", message.partition(), message.offset(), message.key(),
+					message.value());
+		}
+		log.info("Batch Completed");
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		/*CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("devs4j-topic", "Sample Message"); // .get() for Sync
+		// .get(100, TimeUnit.MILLISECONDS) If not TimeoutException.
+		future.whenComplete((result, ex) -> {
+			log.info("Message sent", result.getRecordMetadata().offset());
+		});*/
+		for (int i=0; i<100; i++) {
+			kafkaTemplate.send("devs4j-topic", String.valueOf(i), String.format("Sample Message %d", i));
+		}
+	}
 }
--- src/main/java/com/kafka/spring/config/KafkaConfiguration.java
+++ src/main/java/com/kafka/spring/config/KafkaConfiguration.java
@@ -24,8 +24,8 @@ public class KafkaConfiguration {
 /*        props.put(ProducerConfig.RETRIES_CONFIG, 0);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
         props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);*/
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);*/
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
     }
@@ -45,7 +45,7 @@ public class KafkaConfiguration {
         /* props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");*/
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
     }
@@ -55,11 +55,13 @@ public class KafkaConfiguration {
         return new DefaultKafkaConsumerFactory<>(consumerProperties());
     }
 
-    @Bean
+    @Bean(name = "listenerContainerFactory")
     public ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String>
                 listenerFactory = new ConcurrentKafkaListenerContainerFactory<>();
         listenerFactory.setConsumerFactory(consumerFactory());
+        listenerFactory.setBatchListener(true); // Batch Listeners
+        listenerFactory.setConcurrency(3);
         return listenerFactory;
     }
 }
